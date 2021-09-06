9036816b418efb76ecb6bd6a837ded2e50a00c38
Kyle Roberts <kyle.r@techie.com>
2021-09-06 12:11:45 +0100

    Create other products

---
+++ src/classes/product/products/Clothing.java
@@ -0,0 +1,48 @@
+package classes.product.products;
+
+import classes.product.Product;
+
+public class Clothing extends Product {
+    private String measurement;
+
+    /**
+     * Empty Constructor
+     */
+    public Clothing() {
+        super();
+        measurement = "";
+    }
+
+    /**
+     * Parameter Constructor
+     * @param name - Clothing Name
+     * @param price - Clothing Price
+     * @param stockLevel - Clothing Stock Level
+     * @param measurement - Clothing Measurement
+     */
+    public Clothing(String name, double price, int stockLevel, String measurement) {
+        super(name, price, stockLevel);
+        this.measurement = measurement;
+    }
+
+    /**
+     * Parameter Constructor
+     * @param id - Clothing ID
+     * @param name - Clothing Name
+     * @param price - Clothing Price
+     * @param stockLevel - Clothing Stock Level
+     * @param measurement - Clothing Measurement
+     */
+    public Clothing(int id, String name, double price, int stockLevel, String measurement) {
+        super(id, name, price, stockLevel);
+        this.measurement = measurement;
+    }
+
+    public void setMeasurement(String measurement) {
+        this.measurement = measurement;
+    }
+
+    public String getMeasurement() {
+        return measurement;
+    }
+}
---
+++ src/classes/product/products/Footwear.java
@@ -0,0 +1,47 @@
+package classes.product.products;
+
+import classes.product.Product;
+
+public class Footwear extends Product {
+    private int size;
+
+    /**
+     * Empty Constructor
+     */
+    public Footwear() {
+        super();
+    }
+
+    /**
+     * Parameter Constructor
+     * @param name - Footwear Name
+     * @param price - Footwear Price
+     * @param stockLevel - Footwear Stock Level
+     * @param size - Footwear Size
+     */
+    public Footwear(String name, double price, int stockLevel, int size) {
+        super(name, price, stockLevel);
+        this.size = size;
+    }
+
+    /**
+     * Parameter Constructor
+     * @param id - Footwear ID
+     * @param name - Footwear Name
+     * @param price - Footwear Price
+     * @param stockLevel - Footwear Stock Level
+     * @param size - Footwear Size
+     */
+    public Footwear(int id, String name, double price, int stockLevel, int size) {
+        super(id, name, price, stockLevel);
+        this.size = size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
